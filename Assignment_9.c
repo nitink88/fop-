219ce4825a3bb5f727abe1dc8dfe3b4d045e4091
nitink88 <nitink8807@gmail.com>
2026-04-15 12:02:45 +0530

    Add files via upload

---
+++ Assignment_9.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n, original, rem;
+    int sum = 0;
+
+    printf("Enter a 3-digit number: ");
+    scanf("%d", &n);
+
+    original = n;
+
+    while (n > 0) {
+        rem = n % 10;
+        sum = sum + (rem * rem * rem);
+        n = n / 10;
+    }
+
+    if (sum == original)
+        printf("Armstrong number\n");
+    else
+        printf("Not an Armstrong number\n");
+
+    return 0;
+}
